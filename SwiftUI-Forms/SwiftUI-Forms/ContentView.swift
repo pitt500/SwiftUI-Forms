5908a4425c1aecc27697545454fe95c62468cd1c
pitt500 <pedro.rojaslo50@gmail.com>
2019-12-19 11:20:59 -0700

    Configure base of form

--- SwiftUI-Forms/SwiftUI-Forms/ContentView.swift
+++ SwiftUI-Forms/SwiftUI-Forms/ContentView.swift
@@ -9,13 +9,58 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        Text("Hello, World!")
+  
+  @State private var isScheduled = false
+  @State private var isEnableUntilTomorrow = false
+  @State private var sliderValue: Double = 0
+  
+  var body: some View {
+    NavigationView {
+      Form {
+        Section(header: Text("Night shift automatically shift colors Night shift automatically shift colors Night shift automatically shift colors Night shift automatically shift colors")) {
+          Toggle(isOn: $isScheduled) {
+            Text("Scheduled")
+          }
+          VStack {
+            HStack {
+              Text("From")
+              Spacer()
+              Text("Sunset")
+                .foregroundColor(Color.blue)
+            }
+            
+            HStack {
+              Text("To")
+              Spacer()
+              Text("Sunrise")
+                .foregroundColor(Color.blue)
+            }
+          }
+        }
+        
+        Section {
+          Toggle(isOn: $isEnableUntilTomorrow) {
+            Text("Manually Enable Until Tomorrow")
+          }
+        }
+        
+        Section(header: Text("COLOR TEMPERATURE")) {
+          VStack {
+            HStack {
+              Text("Less Warm")
+              Spacer()
+              Text("More Warm")
+            }
+            Slider(value: $sliderValue)
+          }
+        }
+      }
     }
+  }
 }
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+  static var previews: some View {
+    ContentView()
+  }
 }
